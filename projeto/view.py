0e5e4fef46ebe876f3230e8c5afaa3af20cdf33c
Arthur Amaral <arthuramaralx@outlook.com>
2026-05-04 21:18:51 -0300

    testando commit do github

--- projeto/view.py
+++ projeto/view.py
@@ -7,3 +7,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home.html", context={"name": "Arthur"})
+ 
+ def testingithub(request):
+    return HttpResponse("Testando o github")
